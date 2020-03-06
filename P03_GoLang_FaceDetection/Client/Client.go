48aab576389ac2445d0222698549a69710358c48
phuongbc20 <phuongbc20@gmail.com>
2020-03-06 19:55:23 +0700

    face

---
+++ P03_GoLang_FaceDetection/Client/Client.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"mime/multipart"
+	"net/http"
+	"os"
+)
+
+//--------------------------------------
+//Function MAIN
+//--------------------------------------
+func main() {
+	//--------------------------------------
+	var Path, Type string
+	input(&Path, &Type)
+	//--------------------------------------
+	MakeRequest(Path, Type)
+
+}
+
+//--------------------------------------
+//Function input
+//--------------------------------------
+func input(Path *string, Type *string) {
+	cin := bufio.NewScanner(os.Stdin)
+	fmt.Print("Path(vd: C:/Users/ASUS/go/src/Final/image/in.jpg ) :")
+	cin.Scan()
+	*Path = cin.Text()
+	fmt.Print("Type(1:Image || 2:Json ): ")
+	cin.Scan()
+	*Type = cin.Text()
+}
+
+//--------------------------------------
+//Function tao request
+//--------------------------------------
+
+func MakeRequest(PathFile string, Type string) {
+	url := "http://localhost:8080"
+	method := "POST"
+
+	payload := &bytes.Buffer{}
+	writer := multipart.NewWriter(payload)
+	file, errFile1 := os.Open(PathFile)
+	defer file.Close()
+	part1,
+		errFile1 := writer.CreateFormFile("File", "input.png")
+	_, errFile1 = io.Copy(part1, file)
+	if errFile1 != nil {
+
+		fmt.Println(errFile1)
+	}
+	_ = writer.WriteField("Type", Type)
+	err := writer.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+	res, err := client.Do(req)
+	/*var result map[string]interface{}
+	json.NewDecoder(res.Body).Decode(&result)
+	fmt.Println(result)*/
+	//header
+	if Type == "1" {
+		f, _ := os.Create("Image/output.jpg")
+		_, _ = io.Copy(f, res.Body)
+	} else if Type == "2" {
+		f, _ := os.Create("Json/output.json")
+		_, _ = io.Copy(f, res.Body)
+	}
+}
---
+++ P03_GoLang_FaceDetection/Server/Server.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+//--------------------------------------
+//Function MAIN
+//--------------------------------------
+func main() {
+	http.HandleFunc("/", Action)
+	http.ListenAndServe(":8080", nil)
+}
+
+//--------------------------------------
+//Function MAIN
+//--------------------------------------
+func Action(w http.ResponseWriter, r *http.Request) {
+	r.ParseMultipartForm(10 * 1024 * 1024)
+	_, a, _ := r.FormFile("File")
+	f, _ := a.Open()
+	tempFile, _ := ioutil.TempFile("Uploads", "input-*.jpg")
+	defer tempFile.Close()
+	fileBytes, _ := ioutil.ReadAll(f)
+	tempFile.Write(fileBytes)
+	cmd := exec.Command("pigo", "-in", tempFile.Name(), "-json", "-out", "out.jpg", "-cf", "C:/Users/ASUS/go/src/github.com/esimov/pigo/cascade/facefinder")
+	_ = cmd.Run()
+	fmt.Println(tempFile.Name())
+	//-----------------------------
+	var FileName string
+	if r.FormValue("Type") == "1" {
+		FileName = "out.jpg"
+	} else if r.FormValue("Type") == "2" {
+		FileName = "output.json"
+	}
+	fmt.Println(FileName)
+	file, err := os.Open(FileName)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer file.Close()
+
+	io.Copy(w, file)
+
+}
